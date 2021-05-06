859ef315fd1a77514466382167cb1814c528a508
Bryan Wood <itanex@gmail.com>
2021-05-06 01:12:28 -0700

    Fetch and apply option data

--- src/background.js
+++ src/background.js
@@ -1,10 +1,27 @@
 
 const newTab = "chrome://newtab";
+const data = {
+    blockedUrl: ''
+};
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restoreOptions() {
+
+    // Fetch options data record from storage
+    chrome.storage.sync.get(data, (results) => console.log('background::', results));
+}
+
+function updateOptions(changes, area) {
+    console.log('data was updated', {changes, area});
+    data.blockedUrl = changes.blockedUrl.newValue;
+}
+
 
 async function closeTab(tab) {
     const url = tab.url || tab.pendingUrl;
 
-    if (url && url == "https://javascript.info/coding-style") {
+    if (url && url == data.blockedUrl) {
         await chrome.tabs.remove(tab.id);
     }
     else {
@@ -15,10 +32,13 @@ async function closeTab(tab) {
 async function setTabAsNewTab(tab) {
     const url = tab.url || tab.pendingUrl;
 
-    if (url && url == "https://javascript.info/coding-style") {
+    if (url && url == data.blockedUrl) {
         await chrome.tabs.update(tab.id, { url: newTab });
     }
 }
 
+// document.addEventListener('DOMContentLoaded', );
+restoreOptions()
 // (tab: Tab) => {...}
+chrome.storage.onChanged.addListener(updateOptions)
 chrome.tabs.onCreated.addListener(setTabAsNewTab);
