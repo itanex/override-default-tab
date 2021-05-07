3967e9c6b7e86ffd5610a28db9cdcdc5519cd8a3
Bryan Wood <itanex@gmail.com>
2021-05-06 22:54:54 -0700

    Fix formatting issues

--- src/index.js
+++ src/index.js
@@ -4,59 +4,59 @@ let blockedList = [];
  * Saves options to chrome.storage
  */
 function save_options() {
-    const url = document.getElementById('url').value;
+  const url = document.getElementById('url').value;
 
-    if (!blockedList.includes(url)) {
-        blockedList.push(url);
+  if (!blockedList.includes(url)) {
+    blockedList.push(url);
 
-        chrome.storage.sync.set(
-            { blockedList },
-            () => {
-                displayStatus('Options saved.');
-                renderEntries();
-            });
-    }
+    chrome.storage.sync.set(
+      { blockedList },
+      () => {
+        displayStatus('Options saved.');
+        renderEntries();
+      });
+  }
 }
 
 /**
  * Display provided text in status bar
- * @param {string} displayText 
+ * @param {string} displayText
  * @param {function} callback (optional)
  */
 function displayStatus(displayText, callback) {
-    // Update status to let user know options were saved.
-    const status = document.getElementById('status');
-    status.textContent = displayText;
+  // Update status to let user know options were saved.
+  const status = document.getElementById('status');
+  status.textContent = displayText;
 
-    const loader = document.getElementById('loader');
-    loader.classList.remove('d-none');
+  const loader = document.getElementById('loader');
+  loader.classList.remove('d-none');
 
-    // Clear status text after a moment
-    setTimeout(() => {
-        loader.classList.add('d-none');
-        status.textContent = '';
-        callback?.call();
-    }, 750);
+  // Clear status text after a moment
+  setTimeout(() => {
+    loader.classList.add('d-none');
+    status.textContent = '';
+    callback?.call();
+  }, 750);
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restoreOptions() {
-    // Fetch options data record from storage
-    chrome.storage.sync.get(
-        { blockedList },
-        (results) => {
-            displayStatus('Retrieving Options...');
-            blockedList = [...results.blockedList];
-            renderEntries();
-        }
-    );
+  // Fetch options data record from storage
+  chrome.storage.sync.get(
+    { blockedList },
+    (results) => {
+      displayStatus('Retrieving Options...');
+      blockedList = [...results.blockedList];
+      renderEntries();
+    }
+  );
 }
 
 function renderEntries() {
-    const listElm = document.getElementById('list');
+  const listElm = document.getElementById('list');
 
-    const template = (label) => `
+  const template = (label) => `
     <div class="d-flex flex-row border">
       <div class="border p-1 icon flex-shrink-1">
         X
@@ -78,7 +78,7 @@ function renderEntries() {
     </div>
     `;
 
-    listElm.innerHTML = blockedList.map(x => template(x)).join('');
+  listElm.innerHTML = blockedList.map(x => template(x)).join('');
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
